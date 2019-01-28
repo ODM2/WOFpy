0ac48c49760b3dcf93727a84472def190160ca79
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-01-28 21:44:15 +0000

    Update docs after building Travis build 388 of ODM2/WOFpy
    The docs were built from the branch 'master' against the commit
    956b38e41e1a3e67d7e003bdd5e67ee88a106192.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/ODM2/WOFpy/jobs/485613633.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/DOCTR/lib/python3.7/site-packages/doctr/__main__.py deploy --sync --no-require-master --built-docs docs/Sphinx/_build/html docs-master

--- docs-master/_static/documentation_options.js
+++ docs-master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.0+38.gcb89f71',
+    VERSION: '2.3.0+39.g956b38e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
